c1e199a2ed47911767ec483e3412eecdf4af5945
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-06-23 11:17:38 -0300

    [ ADD ] Prepara o component do Conteudo

--- web/src/components/Player.tsx
+++ web/src/components/Player.tsx
@@ -1,7 +1,22 @@
 export function Player() {
-    return(
+    return (
         <div className="flex-1">
-            Videos
+            <div className="bg-black">
+                <div className="h-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+            </div>
+            <div className="p-8 max-w-[1100px] mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1 className="text-2xl font-bold">Aula 01 - Abertura do Ignite Lab</h1>
+                        <p className="mt-4 text-gray-200">
+                            Participe da abertura do Ignite Lab e descubra na prática a biblioteca JavaScript utilizada por grandes empresas na construção de interfaces web. E vamos juntos, nessa semana, acelerar sua evolução na programação 🚀
+                        </p>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     )
 }
